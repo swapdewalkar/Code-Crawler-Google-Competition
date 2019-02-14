5e16bbd87e65db778b172e720f8b112200405895
swapdewalkar <swapdewalkar@gmail.com>
2019-02-14 14:46:48 +0530

    Added Error Handling. For page fetch again and again. For code try once, if error display username and link

--- crawl_code.py
+++ crawl_code.py
@@ -5,6 +5,7 @@ import urllib.request
 import json
 import sys
 
+#If you are getting error use difference token
 
 NO_OF_USER_ON_ONE_PAGE=30
 
@@ -12,9 +13,10 @@ if len(sys.argv)==1:
 	print("Usage: python crawl_code.py OUTPUT_FOLDER")
 
 output_folder=sys.argv[1]
-token="MjdhMTU1ZWJhMjQxYzBhZjE1OTBiNDVjNTliMzIwNDZ8fDE1NTAwODk1NDY2NDcyMzI="
+token="M2VlMzg3NzY1NmEyODlkMzg2MzdlMzhmYjlkOTAzN2Z8fDE1NTAxMzU0NjA3OTEyNjg%3D"
 problem_id="5753053697277952"
 contest_id="4314486"
+set_id=0  # 0 for small and 1 for large
 no_of_participants=5950
 FLAG=1 # make 0 if you want to save without username
 
@@ -29,7 +31,7 @@ def get_page_links(NO_OF_PARTICIPANTS,contest_id,token):
 def get_user_links(usernames,contest_id,token,problem_id):
 	links=[]
 	for username in usernames:
-		links+=['https://code.google.com/codejam/contest/'+str(contest_id)+'/scoreboard/do/?cmd=GetSourceCode&problem='+str(problem_id)+'&io_set_id=0&username='+username+'&csrfmiddlewaretoken='+str(token)]
+		links+=['https://code.google.com/codejam/contest/'+str(contest_id)+'/scoreboard/do/?cmd=GetSourceCode&problem='+str(problem_id)+'&io_set_id='+str(set_id)+'&username='+username+'&csrfmiddlewaretoken='+str(token)]
 	print("No of usernames found: "+str(len(links)))
 	return links
 
@@ -37,21 +39,32 @@ pagelinks=get_page_links(no_of_participants,contest_id,token)
 usernames=[]
 i=1
 for link in pagelinks:
-	print("Fetching Page "+str(i)+"/"+str(len(pagelinks)))
-	res=json.load(urlopen(link))
-	for row in res['rows']:
-		usernames+=[row['n']]
-	i += 1
-	break
+	while True:
+		try:
+			print("Fetching Page "+str(i)+"/"+str(len(pagelinks)))
+			res=json.load(urlopen(link))
+			for row in res['rows']:
+				usernames+=[row['n']]
+			i += 1
+		except KeyboardInterrupt:
+			exit()
+		except :
+			continue
+		break
+
 os.mkdir(output_folder)
 code_no=1
 userlinks=get_user_links(usernames,contest_id,token,problem_id)
 for link,username in zip(userlinks,usernames):
-	if FLAG==1:
-		name = username
-	else:
-		name= "Code"+str(code_no)
-	print("Fetching code "+str(code_no)+"/"+str(len(userlinks)))
-	urllib.request.urlretrieve(link,output_folder+"/"+name+".zip")
-	code_no+=1
-	
+	try:
+		if FLAG==1:
+			name = username
+		else:
+			name= "Code"+str(code_no)
+		print("Fetching code "+str(code_no)+"/"+str(len(userlinks)))
+		urllib.request.urlretrieve(link,output_folder+"/"+name+".zip")
+		code_no+=1
+	except KeyboardInterrupt:
+			exit()
+	except:
+		print("Error for user: "+username+" Link:"+link)
